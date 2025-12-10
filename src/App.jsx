acb177df83a5de42aee00734210bb83bee24a074
Bernardo Rolim <rolimbernardo6@gmail.com>
2025-12-10 16:20:06 -0300

    Atualizando main

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import "./index.css";
 import { LoadingScreen } from "./components/LoadingSreen";
 import { NavBar } from "./components/NavBar";
 import { MobileMenu } from "./components/MobileMenu";
-import { Home } from "./components/sections/Home";
-import { About } from "./components/sections/About";
-import { Projects } from "./components/sections/Projects";
-import { Contact } from "./components/sections/Contact";
-import { FindMe } from "./components/sections/FindMe";
-import SnakeLinesBackground from "./components/common/SnakeLinesBackground";
+import RotatingFlowersBackground from "./components/common/RotatingFlowersBackground";
+
+import HomePage from "./pages/HomePage";
+import ShopPage from "./pages/ShopPage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -22,17 +24,32 @@ function App() {
       <div
         className={`min-h-screen transition-opacity duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
-        } bg-black text-gray-100 relative`}
+        } bg-[#F5EBD8] text-gray-800 relative`}
       >
-        <SnakeLinesBackground/>
+        <RotatingFlowersBackground />
         <div className="relative z-10">
-          <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} setLanguage={setLanguage} language={language}/>
-          <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} setLanguage={setLanguage} language={language}/>
-          <Home language={language}/>
-          <About language={language}/>
-          <Projects language={language}/>
-          <FindMe language={language}/>
-          <Contact language={language}/>
+          <NavBar
+            menuOpen={menuOpen}
+            setMenuOpen={setMenuOpen}
+            setLanguage={setLanguage}
+            language={language}
+          />
+          <MobileMenu
+            menuOpen={menuOpen}
+            setMenuOpen={setMenuOpen}
+            setLanguage={setLanguage}
+            language={language}
+          />
+
+          <main>
+            <Routes>
+              <Route path="/" element={<HomePage language={language} />} />
+              <Route path="/shop" element={<ShopPage language={language} />} />
+              <Route path="/about" element={<AboutPage language={language} />} />
+              <Route path="/contact" element={<ContactPage language={language} />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </main>
         </div>
       </div>
     </>
