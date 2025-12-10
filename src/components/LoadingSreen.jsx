35ec08da6e2d90dc2e4f82449458aa0b0755b801
Bernardo Rolim <rolimbernardo6@gmail.com>
2025-12-10 16:28:14 -0300

    Atualizando o loading screen

--- src/components/LoadingSreen.jsx
+++ src/components/LoadingSreen.jsx
@@ -2,34 +2,28 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({onComplete}) => {
     const [text, setText] = useState("");
-    // const fullText = "Yokoso, Watashi no ryōiki Tenkai e";
-    const fullText = "<Hello World/>";
+    const fullText = "FloriCult";
 
     useEffect(() => {
         let index = 0;
         const interval = setInterval(() => {
             setText(fullText.substring(0, index));
             index++;
-
             if (index > fullText.length) {
                 clearInterval(interval);
-
                 setTimeout(() => {
                     onComplete();
                 }, 1000);
             }
         }, 100);
-
         return () => clearInterval(interval);
     }, [onComplete]);
     
   return (
-    <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
-      <div className="mb-4 text-4xl font-mono font-bold bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right uppercase">{text}<span className="animate-blink ml-1">|</span></div>
-      <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
-        <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar">
-            {/* {""} */}
-        </div>
+    <div className="fixed inset-0 z-50 bg-[#F5EBD8] text-black flex flex-col items-center justify-center">
+      <div className="mb-4 text-4xl font-mono font-bold leading-right uppercase">{text}<span className="animate-blink ml-1">|</span></div>
+      <div className="w-[200px] h-[2px] bg-[#E8D8B9] rounded relative overflow-hidden">
+        <div className="w-[40%] h-full bg-yellow-500 shadow-[0_0_15px_#FFD700] animate-loading-bar" />
       </div>
     </div>
   );
