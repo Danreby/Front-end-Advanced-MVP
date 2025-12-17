85ef8f10541351615e52845b4c679efa3a3ccb57
Bernardo Rolim <112499709+Danreby@users.noreply.github.com>
2025-12-17 17:50:17 -0300

    Alterando a cor do mobile menu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -35,33 +35,33 @@ export const MobileMenu = ({ menuOpen, setMenuOpen, setLanguage, language }) => 
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full h-full bg-black z-40 flex flex-col items-center justify-center transition-all duration-300 ease-in-out 
+      className={`fixed top-0 left-0 w-full h-full bg-[#F5EBD8] border-b border-[#E8D8B9] z-40 flex flex-col items-center justify-center transition-all duration-300 ease-in-out 
         ${menuOpen
           ? "h-screen opacity-100 pointer-events-auto"
           : "h-0 opacity-0 pointer-events-none"
       }`}
     >
-      <button onClick={() => setMenuOpen(false)} className="absolute top-6 right-6 text-white text-3xl focus:outline-none cursor-pointer"
+      <button onClick={() => setMenuOpen(false)} className="absolute top-6 right-6 text-black text-3xl focus:outline-none cursor-pointer"
         aria-label="Close Menu" title="Close Menu"
         >
         &times;
       </button>
 
-        <a href="/"  onClick={() => setMenuOpen(false)} className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+        <a href="/"  onClick={() => setMenuOpen(false)} className={`text-2xl font-semibold text-black my-4 transform transition-transform duration-300
             ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}> {home} </a>
-        <a href="/about"  onClick={() => setMenuOpen(false)} className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+        <a href="/about"  onClick={() => setMenuOpen(false)} className={`text-2xl font-semibold text-black my-4 transform transition-transform duration-300
             ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}> {about} </a>
-        <a href="/shop"  onClick={() => setMenuOpen(false)} className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+        <a href="/shop"  onClick={() => setMenuOpen(false)} className={`text-2xl font-semibold text-black my-4 transform transition-transform duration-300
             ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}> {projects} </a>
-        <a href="/contact"  onClick={() => setMenuOpen(false)} className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+        <a href="/contact"  onClick={() => setMenuOpen(false)} className={`text-2xl font-semibold text-black my-4 transform transition-transform duration-300
             ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}> {contact} </a>
-        <select className={`bg-transparent text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+        <select className={`bg-transparent text-2xl font-semibold text-black my-4 transform transition-transform duration-300
             ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"} `}
           defaultValue="pt"
           onChange={handleLangChange}
         >
-          <option className="bg-[rgba(10,10,10,0.8)] text-white" value="pt" > Português </option>
-          <option className="bg-[rgba(10,10,10,0.8)] text-white" value="en" > English </option>
+          <option className="text-black" value="pt" > Português </option>
+          <option className="text-black" value="en" > English </option>
         </select>
     </div>
   );
