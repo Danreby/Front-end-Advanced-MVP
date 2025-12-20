2f51eea71c0571f6377ff1a0b09531009e2dce68
Bernardo Rolim <112499709+Danreby@users.noreply.github.com>
2025-12-20 13:19:23 -0300

    Corrigindo pro servidor

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Front-end-Advanced-MVP">
       <App />
     </BrowserRouter>
   </StrictMode>,
